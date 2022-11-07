e527fa52001f934d64ea587f5ea9775ff8bab640
Stéfani Carol <stefanicarol@gmail.com>
2022-11-07 17:26:36 -0300

    Estiliza tela de login

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -31,18 +33,19 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Minha página"),
-        centerTitle: true,
-      ),
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(32.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
+              const Icon(
+                Icons.account_circle,
+                size: 180,
+              ),
               TextFormField(
                 decoration: const InputDecoration(
+                  prefixIcon: Icon(Icons.person),
                   hintText: "Usuário",
                 ),
               ),
@@ -51,17 +54,34 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
               TextFormField(
                 decoration: const InputDecoration(
+                  prefixIcon: Icon(Icons.key),
+                  suffixIcon: Icon(Icons.visibility),
                   hintText: "Senha",
+                ),
+              ),
+              const SizedBox(height: 80),
+              ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.black),
+                ),
+                onPressed: () {},
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 25,
+                    horizontal: 260,
+                  ),
+                  child: Text(
+                    "Login".toUpperCase(),
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                 ),
               ),
             ],
           ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
